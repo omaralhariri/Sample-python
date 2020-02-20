e7f57496af57d6e55ecd03697a3920528a326e6e
Omar Alhariri <Omar.alhariri11@gmail.com>
2020-02-20 09:49:52 +0200

    Removed code

--- hello.py
+++ hello.py
@@ -1,23 +1,5 @@
-import math
-import os
-import sys
-from urllib import request
-
 import requests
-
-print(sys.version)
-# print(sys.version_info)
-print(sys.executable)
 
 r = requests.get("http://python.org")
 print(r.status_code)
-
-name = "omar"
-
-
-def main():
-    print("hey {}!".format(name))
-
-
-if __name__ == "__main__":
-    main()
+print(r.ok)
